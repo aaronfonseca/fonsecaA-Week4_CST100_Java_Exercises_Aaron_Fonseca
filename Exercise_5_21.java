4839f84044acf9ba6b023c9f4e525db0c00fe1c6
aaronfonseca <aaronfonseca@gmx.com>
2016-07-12 23:33:50 -0700

    no message

---
+++ Exercise_5_21.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercise_5_21 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.print("Loan Amount:  ");
+		double Loan = input.nextDouble();
+		
+		System.out.print("Number of Years:  ");
+		double Years = input.nextDouble();
+		
+		input.close();
+		
+		System.out.printf("%-15s%-15s%s\n","Interest Rate","Monthly Payment","Total Payment");
+		
+		double interest = 5.000;
+		while(interest <= 8){
+			double monthlyInterestRate = interest / 1200;
+			double monthlyPayment = Loan * monthlyInterestRate / (1
+				- 1 / Math.pow(1 + monthlyInterestRate, Years * 12));
+			System.out.printf("%-15.4f%-15.2f%.2f\n",interest,monthlyPayment,(monthlyPayment * 12) * Years);
+			interest += 0.125;
+		}
+
+	}
+
+}
